4f01b0b2567a0624690764b2d46192ac3684f9c5
Suresh Mainali <masureshho@gmail.com>
2016-07-28 18:13:56 +0545

    Completed angular2 official tutorial upto routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component }       from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+import { HeroService }     from './hero.service';
+import { HeroesComponent } from './heroes.component';
 
 @Component({
   selector: 'my-app',
   template: `
-  <h1>Ng2 App</h1>
-  <h2>Playing with Angular 2</h2>
-  `
+    <h1>{{title}}</h1>
+    <a [routerLink]="['/heroes']" routerLinkActive="active">Heroes</a>
+    <a [routerLink]="['/dashboard']" routerLinkActive="active">Dashboard</a>
+    <router-outlet></router-outlet>
+    `,
+  directives: [ROUTER_DIRECTIVES],
+  providers: [
+    HeroService
+  ],
+  styleUrls:['app/app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
